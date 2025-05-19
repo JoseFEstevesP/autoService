964c267fbdbc21ef8fd4e9db031c6d0aaddb4a32
jose Esteves <joseesteves199930@gmail.com>
2025-05-19 03:16:08 -0400

    feat: añadir componente Modal con estilos y lógica de apertura/cierre

---
+++ src/components/modal/Modal.tsx
@@ -0,0 +1,35 @@
+import { Button } from '../button/Button';
+import type { ModalProps } from './interface';
+import style from './styles.module.scss';
+
+const Modal = ({
+	className = '',
+	children,
+	isOpen,
+	handleClose,
+	title,
+}: ModalProps) => {
+	return (
+		<dialog className={`${className} ${style.modal}`} open={isOpen}>
+			<section
+				className={`${style.modal__content} ${
+					isOpen ? style['modal__content--open'] : ''
+				}`}
+			>
+				<div className={style.modal__header}>
+					<h2 className={style.modal__title}>{title}</h2>
+					<Button
+						className={style.modal__btn}
+						icon={{
+							iconName: 'close',
+							className: `${style.modal__icon}`,
+						}}
+						onClick={handleClose}
+					/>
+				</div>
+				<div className={style.modal__children}>{children}</div>
+			</section>
+		</dialog>
+	);
+};
+export default Modal;
