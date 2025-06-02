43eacd88d7bb8615ff90b33ae36865dcf8253af4
jose Esteves <joseesteves199930@gmail.com>
2025-06-02 03:05:51 -0400

    feat: reestructurar rutas y agregar layouts para las páginas de servicios, acerca de y catálogo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,49 @@
 import { Route, Routes } from 'react-router-dom';
 import Msg from './components/msg/Msg';
+import LayoutD from './layout/layoutD/LayoutD';
+import LayoutOfMenu from './layout/layoutOfMenu/LayoutOfMenu';
+import About from './pages/about/About';
+import Catalog from './pages/catalog/Catalog';
+import Services from './pages/home/components/services/Services';
 import Home from './pages/home/Home';
-import { RoutesD } from './routes/default/Routes';
 export const App = () => {
 	return (
 		<>
 			<Routes>
-				<Route element={<Home />} path='/' />
-				<Route path='/default/*' element={<RoutesD />} />
+				<Route
+					element={
+						<LayoutOfMenu>
+							<Home />
+						</LayoutOfMenu>
+					}
+					path='/'
+				/>
+				<Route
+					path='/services'
+					element={
+						<LayoutD title='Servicios'>
+							<Services />
+						</LayoutD>
+					}
+				/>
+				<Route
+					path='/about'
+					element={
+						<LayoutOfMenu title='Acerca de'>
+							<About />
+						</LayoutOfMenu>
+					}
+				/>
+				<Route path='/catalog'>
+					<Route
+						index
+						element={
+							<LayoutD title='Catálogo de servicios'>
+								<Catalog />
+							</LayoutD>
+						}
+					/>
+				</Route>
 			</Routes>
 			<Msg />
 		</>
