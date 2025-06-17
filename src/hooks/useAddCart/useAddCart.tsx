00570ccfe909ab7e4cb48919e9a8f69f4baef7da
jose Esteves <joseesteves199930@gmail.com>
2025-06-17 00:47:18 -0400

    feat(cart): simplificar el hook useAddCart eliminando la gestión del estado del carrito

--- src/hooks/useAddCart/useAddCart.tsx
+++ src/hooks/useAddCart/useAddCart.tsx
@@ -1,13 +1,8 @@
 import { ContextCart } from '@/context/cart/CartContext';
-import type { ProductType } from '@/pages/catalog/components/servicesAndProducts/types';
 import { useContext } from 'react';
 
 const useAddCart = () => {
-	const { cart, setCart } = useContext(ContextCart);
-
-	const addToCart = (product: ProductType) => {
-		setCart([...(cart || []), product]);
-	};
+	const { addToCart } = useContext(ContextCart);
 
 	return { addToCart };
 };
