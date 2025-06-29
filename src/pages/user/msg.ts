e88e63487eaa2d7027d806150ccc34381ee479b2
jose Esteves <joseesteves199930@gmail.com>
2025-06-29 01:45:14 -0400

    feat(msg): corregir errores de escritura en los placeholders y agregar mensajes de validación para email, nombre, DNI y teléfono

--- src/pages/user/msg.ts
+++ src/pages/user/msg.ts
@@ -5,7 +5,7 @@ export const msg = {
 		invalid: 'Username can only contain letters, numbers, and underscores',
 		input: {
 			label: 'Enter your username',
-			placeHolder: 'Username',
+			placeholder: 'Username',
 			iconName: 'user',
 		},
 	},
@@ -18,12 +18,61 @@ export const msg = {
 		symbol: 'Password must contain at least one special symbol',
 		input: {
 			label: 'Password',
-			placeHolder: 'Password',
+			placeholder: 'Password',
 			iconName: 'password',
 		},
 	},
+	email: {
+		invalid: 'Invalid email address',
+		input: {
+			label: 'Email',
+			placeholder: 'Email',
+			iconName: 'email',
+		},
+	},
+	username: {
+		minLength: 'Username must be at least 3 characters long',
+		maxLength: 'Username cannot be longer than 20 characters',
+		invalid: 'Username can only contain letters, numbers, and underscores',
+		input: {
+			label: 'Username',
+			placeholder: 'Username',
+			iconName: 'user',
+		},
+	},
+	name: {
+		minLength: 'Name is required',
+		maxLength: 'Name cannot be longer than 50 characters',
+		input: {
+			label: 'Name',
+			placeholder: 'Name',
+			iconName: 'user',
+		},
+	},
+	dni: {
+		minLength: 'DNI must be at least 8 characters long',
+		maxLength: 'DNI must not exceed 12 characters',
+		invalid: 'DNI can only contain numbers',
+		input: {
+			label: 'DNI',
+			placeholder: 'DNI',
+			iconName: 'dni',
+		},
+	},
+	phone: {
+		minLength: 'Phone number must be at least 10 digits long',
+		maxLength: 'Phone number must not exceed 15 digits',
+		invalid: 'Phone number can only contain numbers',
+		input: {
+			label: 'Phone Number',
+			placeholder: 'Phone Number',
+			iconName: 'phone',
+		},
+	},
+
 	page: {
 		btn: 'Log in',
+		btnRegister: 'Register',
 		register: 'Sign up',
 		forgotPassword: 'Forgot your password?',
 	},
